e7496bcbdaca9a6b6d73587cf0f78ae94383cead
HP <HP@LAPTOP-0G1LEEOR>
2022-09-06 13:27:40 +0530

    OpenAPI code changes pushed

---
+++ OpenAPI/src/main/java/com/example/model/Employee.java
@@ -0,0 +1,16 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Employee {
+	
+	private String empID;
+	private String empName;
+	private String designation;
+	
+}
